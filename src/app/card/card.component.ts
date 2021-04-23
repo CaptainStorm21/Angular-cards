ab7b474aec57c0d3e736604a355524f03f21eff3
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-04-23 02:24:43 -0700

    built a list of an object with properties
    set up the parent coponent data
    extracted  into child component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-card',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+
+  //inswtructing a child component how to receive data from parent component
+  //1st. import Input on line 1
+
+  @Input() title = '';
+  @Input() imageUrl = '';
+  @Input() handle = '';
+  @Input() alt = '';
+  @Input() action = '';
 
   constructor() { }
 
